87d17f239145ca70d42e82a3499baff7d0441670
Arbaaz <arbaazshareef786@gmail.com>
2017-05-10 19:03:15 -0500

    Vesrsion 1.1

---
+++ app/src/main/java/com/example/arbaaz/hashknowledge/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.arbaaz.hashknowledge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
