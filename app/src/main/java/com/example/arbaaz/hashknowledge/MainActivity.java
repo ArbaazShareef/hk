08ea5b7086c20838aac4e78b46cb15dd2e02ba39
Arbaaz <arbaazshareef786@gmail.com>
2017-05-10 20:41:18 -0500

    Vesrsion 1.2

--- app/src/main/java/com/example/arbaaz/hashknowledge/MainActivity.java
+++ app/src/main/java/com/example/arbaaz/hashknowledge/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.arbaaz.hashknowledge;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_favorites) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                }
+            }
+        });
     }
 }
