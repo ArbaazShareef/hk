b35210e486e1d42db16a77232c91dc6d22380cef
Arbaaz <arbaazshareef786@gmail.com>
2017-05-10 21:20:10 -0500

    Vesrsion 1.3

--- app/src/main/java/com/example/arbaaz/hashknowledge/MainActivity.java
+++ app/src/main/java/com/example/arbaaz/hashknowledge/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.arbaaz.hashknowledge;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
@@ -16,15 +20,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        final mainFragment mainfrag = new mainFragment();
+        final mainFragmentb mainfragb = new mainFragmentb();
+        final FragmentManager fragmentManager = getFragmentManager();
+        final View fragholder = findViewById(R.id.mainFragment);
+
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
                 if (tabId == R.id.tab_favorites) {
-                    // The tab with id R.id.tab_favorites was selected,
-                    // change your content accordingly.
+                    FragmentTransaction transaction = fragmentManager.beginTransaction();
+                    //getFragmentManager().beginTransaction().remove(getFragmentManager().findFragmentById(R.id.mainFragment)).commit();
+                    transaction.replace(R.id.mainFragment, mainfrag);
+                    transaction.addToBackStack(null);
+
+                    // Commit the transaction
+                    transaction.commit();
+                }else if (tabId == R.id.tab_friends) {
+                    FragmentTransaction transaction = fragmentManager.beginTransaction();
+                    transaction.replace(R.id.mainFragment, mainfragb);
+                    transaction.addToBackStack(null);
+
+                    // Commit the transaction
+                    transaction.commit();
                 }
             }
         });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/arbaaz/hashknowledge/mainFragmentb.java
@@ -0,0 +1,20 @@
+package com.example.arbaaz.hashknowledge;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Arbaaz on 5/10/2017.
+ */
+
+public class mainFragmentb extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.temfragb, container, false);
+    }
+}
